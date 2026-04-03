f4d932223ec60e1be9487f0df24b231f55a2f5da
dongeun6072 <dongeun6072@naver.com>
2026-04-03 09:26:26 +0900

    Demo 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.inavi.global.maps.androiddemo"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
